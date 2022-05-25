d5945bc8a28cc58c87a12d485adacd4faa9ffa73
Osamudiame B ogbemudia <macjayskales@yahoo.com>
2022-05-25 02:39:00 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// console.log('Hello World!');
+
+// convert cl to litre
+function ConvertToLitre (litre) {
+  var formulae = litre / 100;
+  return formulae;
+}
+
+// call function
+var t = (ConvertToLitre(20));
+document.write(t + "litre")
